845b2949b9c380ba193cc606e50ec093eed3c784
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-03-24 15:02:51 -0300

    Começando desenvolvimento, preparação de application.properties

---
+++ src/main/java/com/nandes/CalculadorAPI/CalculadorApiApplication.java
@@ -0,0 +1,13 @@
+package com.nandes.CalculadorAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nandes/CalculadorAPI/CalculadorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandes.CalculadorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
