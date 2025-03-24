40e29a5ea73f1e230b24c2dace11910565346587
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-03-24 16:29:16 -0300

    Criado FoodService

--- src/main/java/com/nandes/CalculadorAPI/domain/repository/FoodRepository.java
+++ src/main/java/com/nandes/CalculadorAPI/domain/repository/FoodRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.nandes.CalculadorAPI.domain.models.Food;
 
 public interface FoodRepository extends JpaRepository<Food, Integer>{
-    
+    boolean existsByName(String name);
 }
---
+++ src/main/java/com/nandes/CalculadorAPI/service/FoodService.java
@@ -0,0 +1,11 @@
+package com.nandes.CalculadorAPI.service;
+
+import java.util.List;
+
+import com.nandes.CalculadorAPI.domain.models.Food;
+
+public interface FoodService {
+    Food findById(int id);
+    Food create(Food foodToCreate);
+    List<Food> findAll();
+}
